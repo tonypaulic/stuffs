0dfe77c686042209c49d6e8caa2f22481895137e
tonypaulic <tony.paulic@gmail.com>
2025-07-20 12:18:47 -0400

    code cleanup + add of info parameter

--- xfceGIT.sh
+++ xfceGIT.sh
@@ -9,31 +9,7 @@
 # Last updated: July 16, 2025 (migration to meson)
 
 LOG="$HOME/Development/$(date +%s).xfcegit.log"
-PREFIX="/usr"		# default is /usr/local
-
-###################################################################################
-# directory to hold source files 
-SOURCE_DIR=~/Development/Xfce.git
-mkdir -p $SOURCE_DIR
-cd $SOURCE_DIR
-###################################################################################
-
-####################################################################################
-# support functions
-build () {
-    # function to build package via meson
-    # $1 = package
-    # $2 = extra options
-    echo
-    echo
-    echo "================================================================"
-    echo $1
-    echo "================================================================"
-	cd $SOURCE_DIR/$1	
-	meson setup --reconfigure build --prefix=$PREFIX $2
-	meson compile -C build
-	sudo meson install -C build
-}
+PREFIX="/usr"			# default is /usr/local
 
 ####################################################################################
 # list of Xfce core components to build
@@ -104,11 +80,96 @@ xXFCE_PLUGINS=""
 xTHUNAR_PLUGINS=""
 xXFCE_APPS=""
 
+###################################################################################
+# directory to hold source files 
+SOURCE_DIR=~/Development/Xfce.git
+mkdir -p $SOURCE_DIR
+cd $SOURCE_DIR
+###################################################################################
+
+####################################################################################
+# support functions
+
+build () {
+	# function to build package via meson
+	# $1 = package
+	# $2 = extra options
+    echo
+    echo
+    echo "================================================================"
+    echo $1
+    echo "================================================================"
+	cd $SOURCE_DIR/$1	
+	meson setup --reconfigure build --prefix=$PREFIX $2
+	meson compile -C build
+	sudo meson install -C build
+}
+
+version () {
+	# function to return version numbers of components
+	# $1 is component name
+	D=$(echo $1 | sed 's/.git//')
+
+	case $D in
+		xfce4-dev-tools|exo|xfdashboard)
+			cat $SOURCE_DIR/$D/configure.ac | grep XDT_VERSION | awk -F '[' '{print " "$2"-"$3}' | tr -d '],) '
+		;;
+		*)
+			[[ -f $SOURCE_DIR/$D/meson.build ]] && cat $SOURCE_DIR/$D/meson.build | grep version | sed 's/license//' | awk -F':' '{print $2}' | tr -d "'," | head -1
+		;;
+	esac
+}
+	
+
 ##################################################################################
 
 case $1 in
-    log)
+
+    info)
+		echo "================================================================"
+        echo "Current Xfce Component Versions:"
+		echo "================================================================"
+		echo "Xfce Core"
+		for x in $XFCE_CORE
+		do
+			v=$(version $x)	
+			echo "  $x = $v" | sed -e 's/.git//'
+		done
+
+		echo "Panel Plugins"
+		for x in $XFCE_PLUGINS
+		do
+			v=$(version $x)			
+			echo "  $x = $v" | sed -e 's/.git//'
+		done
+
+		echo "Thunar Plugins"
+		for x in $THUNAR_PLUGINS
+		do
+			v=$(version $x)			
+			echo "  $x = $v" | sed -e 's/.git//'
+		done
+
+		echo "Xfce Apps"
+		for x in $XFCE_APPS
+		do
+			v=$(version $x)			
+			echo "  $x = $v" | sed -e 's/.git//'
+		done
+		;;
+	
+	log)
         [[ $2 -gt 0 ]] && NUM=$2 || NUM=10
+
+		if [ -n $3 ]; then
+			cd $SOURCE_DIR/$3 || exit 1
+	        echo "========== $3 =========="
+	        git log --graph --pretty=format:"%h%x09%ad  %s" --date=short | grep -v I18n | head -$NUM 
+	        echo ""
+	        cd ..
+			exit 0
+		fi
+
         for d in *; do 
             cd $d
             echo "========== $d =========="
@@ -118,6 +179,7 @@ case $1 in
         done |& less -F
         exit 0    
         ;;
+
     init)
         ###INITIAL INSTALL ONLY
         ######clone the packages
@@ -163,14 +225,14 @@ case $1 in
             libkeybinder3 pavucontrol \
             cmake accountsservice \
             taglib \
-	    zeitgeist \
+			zeitgeist \
             python-gobject python-dbus python-pexpect \
             gspell \
             dbus-glib \
             libexif \
             libburn libisofs \
-	    python-psutil \
-	    help2man \
+			python-psutil \
+			help2man \
             libxrandr libxss xmlto \
             vte3 \
             libxmu \
@@ -260,15 +322,8 @@ case $1 in
             xXFCE_PLUGINS=$XFCE_PLUGINS 
             xTHUNAR_PLUGINS=$THUNAR_PLUGINS
             xXFCE_APPS=$XFCE_APPS
-        fi
+		fi
         ###################################################################################
-
-        ###################################################################################
-        # recommended exports
-        #export PKG_CONFIG_PATH="/usr/lib/pkgconfig:$PKG_CONFIG_PATH"
-        #export CFLAGS="-O2 -pipe"
-        ###################################################################################
-
 
         ###################################################################################
         # Lets do it
@@ -298,16 +353,15 @@ case $1 in
             sudo make install
         )
 		
-		### libxfce4util = gtk-doc gobject-introspection
+			### libxfce4util = gtk-doc gobject-introspection
         echo $xXFCE_CORE | grep libxfce4util && build libxfce4util
-		### xfconf = glib2-devel vala
+			### xfconf = glib2-devel vala
         echo $xXFCE_CORE | grep xfconf && build xfconf
         	### libxfce4ui = gtk3 startup-notification libgtop libgudev glade
         echo $xXFCE_CORE | grep libxfce4ui && build libxfce4ui
-		### garcon = 
+			### garcon = 
         echo $xXFCE_CORE | grep garcon && build garcon
-	
-		###
+			###
         echo $xXFCE_CORE | grep exo &&
        (
             echo
@@ -323,27 +377,27 @@ case $1 in
             sudo make install
         )
 
-		### libxfce4windowing = libwnck3 libdisplay-info wayland wlr-protocols
+			### libxfce4windowing = libwnck3 libdisplay-info wayland wlr-protocols
         echo $xXFCE_CORE | grep libxfce4windowing && build libxfce4windowing
-		### xfce4-panel = gtk-layer-shell libdbusmenu-gtk3
-	echo $xXFCE_CORE | grep xfce4-panel && build xfce4-panel
-		### thunar = libnotify
+			### xfce4-panel = gtk-layer-shell libdbusmenu-gtk3
+	    echo $xXFCE_CORE | grep xfce4-panel && build xfce4-panel
+			### thunar = libnotify
         echo $xXFCE_CORE | grep -e "thunar.git " -e "thunar.git$" && build thunar
-		### xfce4-settings = libxklavier libcanberra xf86-input-libinput
+			### xfce4-settings = libxklavier libcanberra xf86-input-libinput
         echo $xXFCE_CORE | grep xfce4-settings && build xfce4-settings
-		### xfce4-session = polkit xorg-iceauth
+			### xfce4-session = polkit xorg-iceauth
         echo $xXFCE_CORE | grep xfce4-session && build xfce4-session
-		### xfwm4 = libxpresent
+			### xfwm4 = libxpresent
         echo $xXFCE_CORE | grep xfwm4 && build xfwm4
-		### xfdesktop = libyaml
+			### xfdesktop = libyaml
         echo $xXFCE_CORE | grep xfdesktop && build xfdesktop
-		###
+			###
         echo $xXFCE_CORE | grep xfce4-appfinder && build xfce4-appfinder
-		### tumbler = gdk-pixbuf2 ffmpegthumbnailer freetype2 libgsf libopenraw poppler-glib libgepub
+			### tumbler = gdk-pixbuf2 ffmpegthumbnailer freetype2 libgsf libopenraw poppler-glib libgepub
         echo $xXFCE_CORE | grep tumbler && build tumbler
-		###
+			###
         echo $xXFCE_CORE | grep thunar-volman && build thunar-volman
-		### xfce4-power-manager = upower
+			### xfce4-power-manager = upower
         echo $xXFCE_CORE | grep xfce4-power-manager && build xfce4-power-manager
 
         ###################################################################################
@@ -352,37 +406,37 @@ case $1 in
         ###################################################################################
         ###################################################################################
 
-		### xfce4-clipman-plugin = qrencode
+			### xfce4-clipman-plugin = qrencode
         echo $xXFCE_PLUGINS | grep xfce4-clipman-plugin && build xfce4-clipman-plugin 
         	###
         echo $xXFCE_PLUGINS | grep xfce4-cpufreq-plugin && build xfce4-cpufreq-plugin
-		###
+			###
         echo $xXFCE_PLUGINS | grep xfce4-docklike-plugin && build xfce4-docklike-plugin
-		### 
+			### 
         echo $xXFCE_PLUGINS | grep xfce4-eyes-plugin && build xfce4-eyes-plugin 
-		###                
+			###                
         echo $xXFCE_PLUGINS | grep xfce4-genmon-plugin && build xfce4-genmon-plugin
-		###
+			###
         echo $xXFCE_PLUGINS | grep xfce4-mailwatch-plugin && build xfce4-mailwatch-plugin
-		### xfce4-mpc-plugin = libmpd
+			### xfce4-mpc-plugin = libmpd
         echo $xXFCE_PLUGINS | grep xfce4-mpc-plugin && build xfce4-mpc-plugin 
-		###        
+			###        
         echo $xXFCE_PLUGINS | grep xfce4-netload-plugin && build xfce4-netload-plugin
-		### xfce4-notes-plugin = gtksourceview4
+			### xfce4-notes-plugin = gtksourceview4
         echo $xXFCE_PLUGINS | grep xfce4-notes-plugin && build xfce4-notes-plugin
-		###
+			###
         echo $xXFCE_PLUGINS | grep xfce4-places-plugin && build xfce4-places-plugin
         	### xfce4-pulseaudio-plugin = libkeybinder3 pavucontrol
         echo $xXFCE_PLUGINS | grep xfce4-pulseaudio-plugin && build xfce4-pulseaudio-plugin
-		###                        
+			###                        
         echo $xXFCE_PLUGINS | grep xfce4-sensors-plugin && build xfce4-sensors-plugin
-		###
+			###
         echo $xXFCE_PLUGINS | grep xfce4-systemload-plugin && build xfce4-systemload-plugin
-		###
+			###
         echo $xXFCE_PLUGINS | grep xfce4-timer-plugin && build xfce4-timer-plugin
-		###
+			###
         echo $xXFCE_PLUGINS | grep xfce4-weather-plugin && build xfce4-weather-plugin
-		### xfce4-whiskermenu-plugin = cmake accountsservice
+			### xfce4-whiskermenu-plugin = cmake accountsservice
         echo $xXFCE_PLUGINS | grep xfce4-whiskermenu-plugin && build xfce4-whiskermenu-plugin
       		###        
         echo $xXFCE_PLUGINS | grep xfce4-xkb-plugin && build xfce4-xkb-plugin
@@ -393,12 +447,12 @@ case $1 in
         ###################################################################################
         ###################################################################################
 
-		###
+			###
         echo $xTHUNAR_PLUGINS | grep thunar-archive-plugin && build thunar-archive-plugin
-		### thunar-media-tags-plugin = taglib
+			### thunar-media-tags-plugin = taglib
         echo $xTHUNAR_PLUGINS | grep thunar-media-tags-plugin && build thunar-media-tags-plugin
-		###
-		# info on setting up samba: http://goodies.xfce.org/projects/thunar-plugins/thunar-shares-plugin
+			###
+			# info on setting up samba: http://goodies.xfce.org/projects/thunar-plugins/thunar-shares-plugin
         echo $xTHUNAR_PLUGINS | grep thunar-shares-plugin && build thunar-shares-plugin
 
         ###################################################################################
@@ -407,37 +461,37 @@ case $1 in
         ###################################################################################
         ###################################################################################
 
-		### catfish = zeitgist
+			### catfish = zeitgist
         echo $xXFCE_APPS | grep catfish && build catfish
-		###
+			###
         echo $xXFCE_APPS | grep gigolo && build gigolo
-		### mousepad = gspell
+			### mousepad = gspell
         echo $xXFCE_APPS | grep mousepad && build mousepad
-		### parole = dbus-glib
+			### parole = dbus-glib
         echo $xXFCE_APPS | grep parole && build parole
-		###
+			###
         echo $xXFCE_APPS | grep ristretto && build ristretto
-		### xfburn = libburn libisofs
+			### xfburn = libburn libisofs
         echo $xXFCE_APPS | grep xfburn && build xfburn
-		###
+			###
         echo $xXFCE_APPS | grep xfce4-dict && build xfce4-dict
-		###
+			###
         echo $xXFCE_APPS | grep xfce4-notifyd && build xfce4-notifyd
-		### xfce4-panel-profiles = python-psutil
+			### xfce4-panel-profiles = python-psutil
         echo $xXFCE_APPS | grep xfce4-panel-profiles && build xfce4-panel-profiles
-		### xfce4-screenshooter = help2man        
+			### xfce4-screenshooter = help2man        
         echo $xXFCE_APPS | grep xfce4-screenshooter && build xfce4-screenshooter
-		### xfce4-screensaver = libxrandr libxss xmlto
+			### xfce4-screensaver = libxrandr libxss xmlto
         echo $xXFCE_APPS | grep xfce4-screensaver && build xfce4-screensaver
          	### xfce4-terminal = vte3
         echo $xXFCE_APPS | grep xfce4-terminal && build xfce4-terminal
-		###
+			###
         echo $xXFCE_APPS | grep xfce4-taskmanager && build xfce4-taskmanager
-		###
+			###
         echo $xXFCE_APPS | grep xfce4-volumed-pulse && build xfce4-volumed-pulse
-		
-  		### xfdashboard = clutter        
-	echo $xXFCE_APPS | grep xfdashboard &&
+			### xfdashboard = clutter
+        
+		echo $xXFCE_APPS | grep xfdashboard &&
         (
             echo
             echo
@@ -492,7 +546,8 @@ case $1 in
         echo "   init -> initialize the build environment MUST BE RUN FIRST!!!"
         echo "   update -> to update the build from the git tree for packages with changes"
         echo "   update-all -> to update all packages regardless of whether there are changes"
-	echo "   log LINES -> display non-translation git commits at LINES lines per package"
+		echo "   log [LINES] [PACKAGE] -> display non-translation git commits at LINES lines per package"
+		echo "   info -> list current package versions"
         echo
         ;;
 esac
